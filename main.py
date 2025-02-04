96f989672917d442cfd0a5e7088c50eec59025f9
staffan-haglund <stahag@gmail.com>
2025-02-04 23:57:44 +0100

    nytt

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from pso import PSO
 
 if __name__ == "__main__":
 
-    new_swarm = PSO(swarm_size=5, dimensions=2, max_iter=100)
+    new_swarm = PSO(swarm_size=5, dimensions=2, max_iter=100, rand_seed=3)
     print(str(new_swarm))
 
     print(new_swarm.get_swarm_specs())
-
     print(new_swarm.get_swarm())
+    print("\nSTART ###############################################")
+    print(f'Antal iterationer: {new_swarm._max_iter}')
 
-    new_swarm.update_particle()
+    new_swarm.get_particle()
+    new_swarm.update_swarm()
+    new_swarm.get_particle()
+
+    # print(f'Particle #0: {new_swarm.get_particle(0)}')
+    
+    # new_swarm.update_particle()
--- pso.py
+++ pso.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import numpy as np
+import matplotlib as plt
 
 from math import cos, pi
 
@@ -54,47 +55,104 @@ class PSO:
         random.seed(rand_seed)
         for i in range(self._swarm_size):
             self._swarm[i] = {'Position': [round(random.uniform(self._xmin, self._xmax), 2) for x in range(self._dimensions)], \
-                              'Velocity': round(random.uniform(self._xmin, self._xmax), 2)}
+                              'Velocity': [round(random.uniform(self._xmin, self._xmax), 2) for v in range(self._dimensions)]}
+        temp_pbest_value = 0.0
         for i in range(self._swarm_size):
             self._swarm[i]['pbest'] = self._swarm[i]['Position']
+            self._swarm[i]['best_value'] = self.rastrigin_fitness(self._swarm[i]['Position'])
+            # Om nytt personbästa (pbest) är bättre än temp_pbest, 
+            # spara pbest som ny global bästa (gbest)
+            # spara ny pbest som ny temp_pbest
+            if abs(self._swarm[i]['best_value']) < abs(temp_pbest_value):
+                self._swarm['gbest'] = self._swarm[i]['pbest']
+                temp_pbest_value = self._swarm[i]['best_value']
+            # Annars, behåll bästa pbest som gbest
+            else:
+                self._swarm['gbest'] = self._swarm[i]['pbest']
+        
+        # Spara gbest_value från den med gbest position
+        self._swarm['gbest_value'] = self.rastrigin_fitness(self._swarm['gbest'])
 
         for i in range(self._swarm_size):
             self._swarm[i]['Fitness'] = self.rastrigin_fitness(self._swarm[i]['Position'])
+
 
     def __str__(self):
         str_info = [f'{self._swarm[particle]}\n' for particle in self._swarm]
         return str(''.join(str_info))
 
+
     def get_swarm(self) -> dict:
         return self._swarm
     
+
     def get_swarm_specs(self):
         return {'Size': self._swarm_size, 'Dimensions': self._dimensions, 'Max iterations': self._max_iter, 
                 'Inertia weight': self._w, 'Cognitive coefficient': self._c1, 'Social coefficient': self._c2, 
                 'Search domain': (self._xmin, self._xmin), 'Function': self._func}
     
+
+    def get_particle(self):
+        print("-----------------------------------")
+        for i in range(self._swarm_size):
+            print(f'Particle #{i}: Position: {self._swarm[i]['Position']} | pbest: {self._swarm[i]['pbest']} | gbest: {self._swarm['gbest']}')
+        # return self._swarm[i]
+
     def sphere_fitness(self, particle):
         fitness = sum((x ** 2) for x in particle)
         return fitness
 
+
     def rastrigin_fitness(self, particle):
-        # for i in range(len(particle)):
-        #     print(f'i: {particle[i]}')
         fitness = 10 * self._dimensions + sum((x ** 2) - 10 * cos(2 * pi * x) for x in particle)
         return fitness
         
 
-    def update_particle(self):
-        for i in range(self._swarm_size):
-            # print(f'{self._swarm[i]['Position']}')  # = {'Velocity': round(random.uniform(self._xmin, self._xmax), 2)}
-            self.rastrigin_fitness(self._swarm[i]['Position'])
-        r1 = round(random.random(), 3)
-        r2 = round(random.random(), 3)
-        # # Update velocity
-        # vi​=w⋅vi​+c1​⋅r1​⋅(pi​−xi​)+c2​⋅r2​⋅(g−xi​)
-        # v = self._w * self._swarm[i]['Velocity'] + self._c1 * r1 * (self._swarm[i]['pbest'] - x) + self._c2 * r2 * (self._swarm[i]['gbest'] - x)
+    def update_swarm(self):
+        print("-----------------------------------")
+        print("UPDATE SWARM")
+        for iteration in range(self._max_iter):
+            # print(f'Iteration #{iteration}')
+            for i in range(self._swarm_size):
+                # Utvärdera nuvarande position
+                current_value = self.rastrigin_fitness(self._swarm[i]['Position'])
+                # Uppdatera personbästa
+                if current_value < self._swarm[i]['best_value']:
+                    self._swarm[i]['pbest'] = self._swarm[i]['Position']
+                    self._swarm[i]['best_value'] = current_value
+                #  Uppdatera globalt bästa
+                if current_value < self._swarm['gbest_value']:
+                    self._swarm['gbest'] = self._swarm[i]['Position']
+                    self._swarm['gbest_value'] = current_value
 
-        v = self._w * self._swarm[i]['Velocity'] + \
-            self._c1 * r1 * (self._swarm[i]['pbest'] - self._swarm[i]['Position']) + \
-                self._c2 * r2 * (self._swarm[i]['gbest'] - self._swarm[i]['Position'])
-    
+            for i in range(self._swarm_size):
+                r1 = round(random.random(), 3)
+                r2 = round(random.random(), 3)
+
+                # Hastighetsuppdatering
+                v_new = []
+                for j in range(self._dimensions):
+                    v_new.append(self._w * self._swarm[i]['Velocity'][j] + \
+                        self._c1 * r1 * (self._swarm[i]['pbest'][j] - self._swarm[i]['Position'][j]) + \
+                            self._c2 * r2 * (self._swarm['gbest'][j] - self._swarm[i]['Position'][j]))
+                self._swarm[i]['Velocity'] = v_new
+                # Positionsuppdatering
+                p_new = []
+                for k in range(self._dimensions):
+                    # particles[i].x = particles[i].x + particles[i].v
+                    p_new.append(self._swarm[i]['Position'][k] + self._swarm[i]['Velocity'][k])
+                self._swarm[i]['Position'] = p_new
+
+    def plot_convergence(self):
+
+
+        plt.figure(figsize=(8, 5))
+        plt.plot(self._max_iter, best_fitness_values, marker='o', linestyle='-', color='b', label="Best fitness")
+
+        plt.xlabel("Iterationer")
+        plt.ylabel("Best Fitness Value")
+        plt.title("PSO Konvergenskurva")
+        plt.legend()
+        plt.grid()
+
+        plt.show()
